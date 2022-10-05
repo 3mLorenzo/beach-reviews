0834b44ee79965f45408a5b7675146453d3fc3bf
3mLorenzo <108633734+3mLorenzo@users.noreply.github.com>
2022-10-05 18:27:04 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+@login_required
